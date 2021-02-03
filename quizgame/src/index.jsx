d7e7872d8d118ee541c9750b7b3f6a83931df223
magnuen2k <maen030@student.kristiania.no>
2021-02-03 20:26:13 +0100

    implement quiz with react

---
+++ quizgame/src/index.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import { getRandomQuiz } from "./quizzes";
+
+// FUNCTIONAL COMPONENT VERSION
+const App = () => {
+    const [quiz, setQuiz] = useState(getRandomQuiz(1)[0]);
+    const [score, setScore] = useState(0);
+
+    const checkAnswer = (answerIndex) => {
+        if (answerIndex == quiz.indexOfCorrectAnswer) {
+            alert("Correct");
+            setScore(score + 1);
+            setQuiz(getRandomQuiz(1)[0]);
+        } else {
+            alert("Try again");
+        }
+    };
+
+    return (
+        <div>
+            <h2>{quiz.question}</h2>
+            <div className="anwers">
+                {quiz.answers.map((answer, index) => (
+                    <button
+                        className="answer"
+                        key={index}
+                        onClick={() => checkAnswer(index)}
+                    >
+                        {answer}
+                    </button>
+                ))}
+            </div>
+            <div className="score">{score}</div>
+        </div>
+    );
+};
+
+// CLASS COMPONENT VERSION
+// class App extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             quiz: getRandomQuiz(1)[0],
+//             score: 0,
+//         };
+//     }
+
+//     checkAnswer(answerIndex) {
+//         if (answerIndex == this.state.quiz.indexOfCorrectAnswer) {
+//             alert("Correct!!");
+//             this.setState((prevState) => ({
+//                 quiz: getRandomQuiz(1)[0],
+//                 score: prevState.score + 1,
+//             }));
+//         } else {
+//             alert("Wrrrrong!!");
+//         }
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h2>{this.state.quiz.question}</h2>
+//                 <div className="anwers">
+//                     {this.state.quiz.answers.map((answer, index) => (
+//                         <button
+//                             className="answer"
+//                             key={index}
+//                             onClick={() => this.checkAnswer(index)}
+//                         >
+//                             {answer}
+//                         </button>
+//                     ))}
+//                 </div>
+//                 <div className="score">{this.state.score}</div>
+//             </div>
+//         );
+//     }
+// }
+
+ReactDOM.render(<App />, document.getElementById("root"));
--- quizgame/webpack.config.js
+++ quizgame/webpack.config.js
@@ -2,12 +2,24 @@ const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: "./src/index.jsx",
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname, "public"),
-        libraryTarget: "var",
-        library: "EntryPoint",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".jsx"],
     },
     devServer: {
         contentBase: "./public",
