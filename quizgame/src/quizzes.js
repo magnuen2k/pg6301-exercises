466f9378b197d2f93ea7253a96d4bd50e771ba51
Magnus Enholm <53129079+magnuen2k@users.noreply.github.com>
2021-02-03 21:09:02 +0100

    Implement code with react (#3)
    * implement quiz with react
    
    * add wrong alert
    
    * always use ===
    
    * add some styling

--- quizgame/src/quizzes.js
+++ quizgame/src/quizzes.js
@@ -2,21 +2,37 @@ const quizzes = [
     {
         question: "Whats nine plus ten?",
         answers: [30, 21, 19, 50],
-        indexOfCorrectAnswer: 1
+        indexOfCorrectAnswer: 1,
     },
     {
         question: "Best CS:GO pro",
-        answers: [
-            "Brax",
-            "Simple",
-            "Coldzera",
-            "M0E TV",
-        ],
-        indexOfCorrectAnswer: 0
+        answers: ["Brax", "Simple", "Coldzera", "M0E TV"],
+        indexOfCorrectAnswer: 0,
     },
-]
+];
 
 // Should take number of quizzes as input
-export const getRandomQuiz = () => {
-    return quizzes[Math.floor(Math.random() * quizzes.length)];
-}
+export const getRandomQuiz = (numberOfQuizzes) => {
+    if (numberOfQuizzes < 1) {
+        throw "Invalid number of requested quizzes: " + n;
+    }
+
+    if (numberOfQuizzes > quizzes.length) {
+        throw "Too many quizzes";
+    }
+
+    const selection = Array(numberOfQuizzes);
+
+    let i = 0;
+    while (i < numberOfQuizzes) {
+        const k = Math.floor(quizzes.length * Math.random());
+        if (selection.includes(k)) {
+            continue;
+        }
+
+        selection[i] = k;
+        i++;
+    }
+
+    return Array.from(selection).map((e) => quizzes[e]);
+};
--- quizgame/tests/quizzes-test.js
+++ quizgame/tests/quizzes-test.js
@@ -1,6 +1,6 @@
 const { getRandomQuiz } = require("../src/quizzes");
 
 test("Test get random quiz", () => {
-    const quiz = getRandomQuiz();
+    const quiz = getRandomQuiz(1)[0];
     expect(quiz.answers.length).toBe(4);
 });
