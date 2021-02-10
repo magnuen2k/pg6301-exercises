3cd466d8306fe2220b7a77ce967865f29e6a0d7e
magnuen2k <maen030@student.kristiania.no>
2021-02-10 19:37:54 +0100

    move code from index to match.jsx

---
+++ quizgame/src/match.jsx
@@ -0,0 +1,76 @@
+import React, {useState} from "react";
+import {getRandomQuiz} from "./quizzes";
+
+const Match = () => {
+    const [quiz, setQuiz] = useState(getRandomQuiz(1)[0]);
+    const [score, setScore] = useState(0);
+
+    const checkAnswer = (answerIndex) => {
+        if (answerIndex === quiz.indexOfCorrectAnswer) {
+            alert("Correct");
+            setScore(score + 1);
+            setQuiz(getRandomQuiz(1)[0]);
+        } else {
+            alert("Try again!");
+        }
+    };
+
+    return (
+        <div className="container">
+            <h2 className="question">{quiz.question}</h2>
+            {quiz.answers.map((answer, index) => (
+                <button
+                    className="answer"
+                    key={index}
+                    onClick={() => checkAnswer(index)}
+                >
+                    {answer}
+                </button>
+            ))}
+            <div className="score">Score: {score}</div>
+        </div>
+    );
+};
+
+export default Match;
+
+// CLASS COMPONENT VERSION
+/*export class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quiz: getRandomQuiz(1)[0],
+            score: 0,
+        };
+    }
+
+    checkAnswer(answerIndex) {
+        if (answerIndex === this.state.quiz.indexOfCorrectAnswer) {
+            alert("Correct!!");
+            this.setState((prevState) => ({
+                quiz: getRandomQuiz(1)[0],
+                score: prevState.score + 1,
+            }));
+        } else {
+            alert("Wrrrrong!!");
+        }
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h2>{this.state.quiz.question}</h2>
+                    {this.state.quiz.answers.map((answer, index) => (
+                        <button
+                            className="answer"
+                            key={index}
+                            onClick={() => this.checkAnswer(index)}
+                        >
+                            {answer}
+                        </button>
+                    ))}
+                <div className="score">Score: {this.state.score}</div>
+            </div>
+        );
+    }
+}*/
