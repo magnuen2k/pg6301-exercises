ea0ca444531e50a033dd7896ab70f3824e71f017
magnuen2k <maen030@student.kristiania.no>
2021-02-17 19:42:04 +0100

    add react router and setup node server with express

--- quizgame/src/client/index.jsx
+++ quizgame/src/client/index.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 import {Match} from "./match";
+import {Home} from "./home";
 
-// FUNCTIONAL COMPONENT VERSION
+const NotFound = () => {
+    return (
+        <div>
+            <p>404 not found</p>
+        </div>
+    )
+}
+
 const App = () => {
     return (
-        <Match/>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/match" component={Match}/>
+                <Route exact path="/" component={Home}/>
+                <Route component={NotFound}/>
+            </Switch>
+        </BrowserRouter>
     );
 };
 
