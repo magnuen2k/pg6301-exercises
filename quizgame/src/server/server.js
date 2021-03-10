c87180266b48021c428829209de836b37cead289
magnuen2k <maen030@student.kristiania.no>
2021-03-10 19:25:34 +0100

    create endpoint for getting quiz objects

--- quizgame/src/server/server.js
+++ quizgame/src/server/server.js
@@ -2,10 +2,27 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const quizzes = [
+    {
+        question: "Whats nine plus ten?",
+        answers: [30, 21, 19, 50],
+        indexOfCorrectAnswer: 1,
+    },
+    {
+        question: "Best CS:GO pro",
+        answers: ["Brax", "Simple", "Coldzera", "M0E TV"],
+        indexOfCorrectAnswer: 0,
+    },
+]
 
 app.use(express.static(path.resolve(__dirname, "..", "..", "dist")));
 
+app.get("/api/quizzes", (req, res, next) => {
+    console.log(quizzes);
+    res.json(quizzes);
+})
 
+// Return correct page
 app.use((req, res, next) => {
     if (req.method !== "GET") {
         return next();
